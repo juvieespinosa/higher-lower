f8387b0687363cb7d4a6a4e5787f95de7135a049
juvieespinosa <jhuv89@gmail.com>
2022-08-28 15:37:56 -0700

    update the random numbers between 0-15

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ print(random_number)
 #The website starts with a main page
 @app.route('/')
 def home():
-    return '<h1>Guess a number between 0 and 9</h1>' \
+    return '<h1>Guess a number between 0 and 15</h1>' \
             '<img src="https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif">'
 
 
